f1066d7fc5e75ddf2177e05498570c7884d75da1
Frannkko <fguzman@cuc.edu.co>
2024-11-27 10:42:37 -0500

    Add files via upload

---
+++ SESION 7/app.js
@@ -0,0 +1,33 @@
+var num1=10
+
+
+if (true) {
+    let num2=5
+    console.log(num2)
+    console.log(num1  )
+}
+let num2=20
+
+console.log(num1)
+console.log(num2)
+const IVA=0.19
+console.log(IVA)
+//operadores artimeticos
+console.log('suma:',num1+num2)
+console.log('resta:',num1-num2)
+console.log('multiplicación:',num1*num2)
+console.log('división:',num1/num2)
+console.log('ecuación:',((num1/num2)*2+num1))
+//operadores de asignación compuestos
+let a=5
+
+a+=2//a=a+2
+console.log(a)
+a-=1//a=a-1
+console.log(a)
+a*=2//a=a*2
+console.log(a)
+a/=3//a=a/3
+console.log(a)
+//imprimir en el documento
+document.write('<h2>Hola desde Js</h2>')
